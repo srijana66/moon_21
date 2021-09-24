50fcfc236af054075e4aaeef00daf949c97f8f25
unknown <student@LABII-010.sddb.student>
2021-09-24 12:09:02 +0545

    intial commit

---
+++ float.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+const float pi=3.14;
+int main()
+{
+	float r,area=0;
+	cout<<"enter the radius of circle:";
+	cin>>area;
+	area=pi*r*r;
+	cout<<"the area of circle is :"<<area<<endl;
+	return 0;
+}
